3c4bdf5151545ebf361bb1c1c0abb46987d6e900
Sahan Kalhara <142345492+SkDev22@users.noreply.github.com>
2025-07-29 23:05:30 +0530

    Create appointments.route.js file
    Crated appointments.route.js file and add routes for appointments.

---
+++ backend/routes/appointments.routes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const {
+  getAllAppointments,
+  getAppointmentsWithDetails,
+  createAppointment,
+  updateAppointment,
+  deleteAppointment,
+  updateAppointmentStatus,
+} = require("../controllers/appointments.controller");
+
+const router = express.Router();
+
+router.get("/", getAllAppointments);
+router.get("/appointmentdetails", getAppointmentsWithDetails);
+router.post("/", createAppointment);
+router.put("/update/:id", updateAppointment);
+router.patch("/update/:id/status", updateAppointment);
+router.delete("/delete/:id", deleteAppointment);
+
+module.exports = router;
